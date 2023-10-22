e87e8fd91ef69488de10f029420ea1ad3dd5782b
Abhishek singh <90918112+abhisheksingh433@users.noreply.github.com>
2023-10-22 15:05:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,123 @@
+import { useRef, useState, useEffect } from 'react';
+import Editor from '@monaco-editor/react';
+import { Code2, Braces, Pencil, Clipboard, Lock } from 'lucide-react';
+import './App.css';
+
+const files = {
+  "index.html": {
+    name: "index.html",
+    language: "html",
+    value: "// write your html code here...",
+  },
+  "index.css": {
+    name: "index.css",
+    language: "css",
+    value: "/* write your css code here... */",
+  },
+  "script.js": {
+    name: "script.js",
+    language: "javascript",
+    value: "// write your javascript code here...",
+  },
+};
+
+function App() {
+  const editorRef = useRef(null);
+  const [text, setText] = useState("");
+  const [filename, setFilename] = useState("index.html");
+  const [editorOptions, setEditorOptions] = useState({ readOnly: false });
+
+  const file = files[filename];
+
+  const handleEditorChange = (newValaue) => {
+    setText(newValaue);
+
+    // Store code in local storage based on the language
+    localStorage.setItem(`editor-${file.language}`, newValaue);
+  };
+
+  function handleEditor(editor, monaco) {
+    editorRef.current = editor;
+  }
+
+  const copyCodeToClipboard = () => {
+    if (editorRef.current) {
+      const codeLines = editorRef.current.getValue();
+
+      const codeWithoutComments = codeLines.replace(/\/\/[^\n]*|\/\*[\s\S]*?\*\//g, '');
+      const pValueset = document.querySelector('#textarea');
+      pValueset.value = codeWithoutComments;
+    }
+  };
+
+  const toggleEditorLock = () => {
+    setEditorOptions({ readOnly: !editorOptions.readOnly });
+  };
+
+  // Load the code from local storage based on the language
+  const loadEditorContent = () => {
+    const storedContent = localStorage.getItem(`editor-${file.language}`);
+    if (storedContent) {
+      setText(storedContent);
+    }
+  };
+
+  // useEffect(() => {
+  //   loadEditorContent();
+  // }, [filename]);
+
+  return (
+    <div className="App">
+      <h1>Code Editor</h1>
+      <div className="change_btn">
+        <button onClick={() => setFilename("index.html")}>
+          <Code2 size={18} id="btn_icon" />
+          HTML
+        </button>
+        <button onClick={() => setFilename("script.js")}>
+          <Braces size={18} id="btn_icon" />
+          JavaScript
+        </button>
+        <button onClick={() => setFilename("index.css")}>
+          <Pencil size={18} id="btn_icon" />
+          CSS
+        </button>
+        <button onClick={copyCodeToClipboard}>
+          <Clipboard size={18} id="btn_icon" />
+          Copy Text
+        </button>
+        <button
+          onClick={toggleEditorLock}
+          className={editorOptions.readOnly ? 'locked' : 'unlocked'}
+        >
+          <Lock size={18} id="btn_icon" />
+          {editorOptions.readOnly ? 'Unlock Editor' : 'Lock Editor'}
+        </button>
+        <button onClick={loadEditorContent}>Load</button>
+      </div>
+      <Editor
+        width="70%"
+        height="70vh"
+        value={text}
+        theme="vs-dark"
+        path={file.name}
+        className="editor_"
+        onMount={handleEditor}
+        onChange={handleEditorChange}
+        defaultLanguage={file.language}
+        defaultValue={`${file.value} \n`}
+        options={{ ...editorOptions, fontSize: 16 }}
+      />
+      <textarea
+        name=""
+        cols="30"
+        rows="10"
+        disabled
+        id="textarea"
+        placeholder="Copy text display here..."
+      ></textarea>
+    </div>
+  );
+}
+
+export default App;
